c7715e2bb6112e6667907c988d2a61f15f297bce
Jacob Fischer <jacob.t.fischer@gmail.com>
2019-12-09 18:35:51 -0600

    correct wget docs script

--- docs/docs.sh
+++ docs/docs.sh
@@ -2,4 +2,4 @@
 
 cd ../
 godoc
-wget -m -k -q -erobots=off -x src/ --no-host-directories --no-use-server-timestamps http://localhost:6060
+wget -m -k -q -erobots=off -X src/ --no-host-directories --no-use-server-timestamps http://localhost:6060
